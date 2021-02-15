0e35ffe71cf73f87ff98bfd412d9b9825b51fc25
dsanchor <dsanchor@redhat.com>
2021-02-15 12:44:42 +0100

    Added some bootstrap utils

---
+++ bootstrap/bootstrap-user.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+MESH_NS=$1
+USER=$2
+USER_NS=$3
+
+echo "Bootstraping user $USER (Istio ns=$MESH_NS)"
+
+echo "Creating namespaces..."
+cat ./ocp/namespaces.yaml | USER_NS=$USER_NS envsubst | oc apply -f -
+
+echo "Adding view role to ns=$MESH_NS"
+cat ./ocp/rb/mesh-ns-rb.yaml | USER=$USER MESH_NS=$MESH_NS USER_NS=$USER_NS envsubst | oc apply -f -
+
+echo "Adding mesh-user role to ns=$MESH_NS"
+cat ./ocp/rb/mesh-user-rb.yaml | USER=$USER USER_NS=$USER_NS  envsubst | oc apply -f - -n $MESH_NS
+
+echo "Adding admin role to its ns"
+cat ./ocp/rb/user-admin.yaml | USER=$USER USER_NS=$USER_NS envsubst | oc apply -f -
---
+++ bootstrap/bootstrap-users-from-list.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+MESH_NS=$1
+USERS_LIST=$2
+
+ADMIN_USER=$3
+OCP_API=$4
+
+oc login -u $ADMIN_USER $OCP_API
+
+for line in `cat $USERS_LIST`
+do
+  IFS='#' read -r -a array <<< "$line"
+  echo "Bootstraping user => ${array[0]}  ${array[1]}"
+  ./bootstrap-user.sh $MESH_NS ${array[0]}  ${array[1]} 
+  echo "-------------------------------"
+done 
